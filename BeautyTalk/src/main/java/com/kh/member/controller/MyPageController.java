e1f5ca9d2cd1be28d37e36d02f43dab2f69a040a
yong <ckddyd6587@naver.com>
2025-03-22 00:54:55 +0900

    마이페이지 수정

--- BeautyTalk/src/main/java/com/kh/member/controller/MyPageController.java
+++ BeautyTalk/src/main/java/com/kh/member/controller/MyPageController.java
@@ -15,7 +15,6 @@ import javax.servlet.http.HttpSession;
 @WebServlet("/myPage.me")
 public class MyPageController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
     /**
      * @see HttpServlet#HttpServlet()
      */
